051697a9bea9da4fec8ea41bdc1654cea714c10b
Yace Sf <yacine.sf@outlook.fr>
2023-10-28 17:43:14 +0200

    Modification du router

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,14 +1,20 @@
 import MainIndex from '../pages/MainIndex/index'
 import MainSignIn from '../pages/MainSignIn/index'
 import MainUser from '../pages/MainUser/index'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function RoutesApp() {
+  const token = localStorage.getItem("loginToken");
+  const isLogged = Boolean(token);
+
   return (
     <Routes>
       <Route path="/" element={<MainIndex />} />
       <Route path="/login" element={<MainSignIn />} />
-      <Route path="/profile" element={<MainUser />} />
+      <Route
+        path="/profile"
+        element={isLogged ? <MainUser /> : <Navigate to="/login" />}
+      />
     </Routes>
   )
 }
